24502cb144feaf57deba919a2bc4cf09a7e32dd3
jenn <jfohring@eos.ubc.ca>
2016-07-12 16:18:31 -0700

    fixed for new spreadsheet

--- code/mosiDriver.m
+++ code/mosiDriver.m
@@ -13,48 +13,58 @@ headerlinesIn = 1;
 T = importdata(filename,delimiterIn,headerlinesIn); 
 
 %% extract locations and elevation of temp stations
+xyz = T.data.Sheet1(:,1:3);
 
-xyzdata = T.data;
-data = data(:,1:3);
-data = unique(data,'rows');
+% for plotting only station locations,
+xyz = unique(xyz,'rows');
 
-Tz = data(:,1);
-Tlat = data(:,2);
-Tlong = data(:,3);
+Tz = xyz(:,1);
+Tlat = xyz(:,2);
+Tlong = xyz(:,3);
+
+%% extract aegypti locations for all dates
+xyz = T.data.Sheet1(:,8:10);
+% remove nan's
+ind = find(isnan(xyz),1,'first')-1;
+Az = xyz(1:ind,1);
+Alat = xyz(1:ind,2);
+Along = xyz(1:ind,3);
+
+%% extract albopitus locations for all dates
+xyz = T.data.Sheet1(:,12:14);
+% remove nan's
+ind = find(isnan(xyz),1,'first')-1;
+Bz = xyz(1:ind,1);
+Blat = xyz(1:ind,2);
+Blong = xyz(1:ind,3);
 
 %% import mosi data locations and elevation
-filename = 'aegypti_CSA_elevation.xlsx';
-delimiterIn = '\t'; % if it's the tab deliminated text file
-headerlinesIn = 1;
-sheet = 1;
+% filename = 'aegypti_CSA_elevation.xlsx';
+% delimiterIn = '\t'; % if it's the tab deliminated text file
+% headerlinesIn = 1;
+% sheet = 1;
+% 
+% % get a structure with headers and text
+% % importdata looks at file extensions and imports accordingly
+% A = importdata(filename,delimiterIn,headerlinesIn); 
+% 
+% %% indexes to Argentina
+% countries = A.textdata.aegypti_CSA_elevation(2:end,1);
+% % get the indexes into the data file for argentina
+% ind = find(not(cellfun('isempty', strfind(countries,'Argentina'))));
+% 
+% % extract mosi data for all dates, for Argentina
+% ArgData = A.data.aegypti_CSA_elevation(ind,:);
+% 
+% % get lat, lon, elevation for all years
+% lat  = ArgData(:,2);
+% long = ArgData(:,3);
+% Z    = ArgData(:,4);
 
-% get a structure with headers and text
-% importdata looks at file extensions and imports accordingly
-A = importdata(filename,delimiterIn,headerlinesIn); 
-
-%% indexes to Argentina
-countries = A.textdata.aegypti_CSA_elevation(2:end,1);
-% get the indexes into the data file for argentina
-ind = find(not(cellfun('isempty', strfind(countries,'Argentina'))));
-
-% extract mosi data for all dates, for Argentina
-ArgData = A.data.aegypti_CSA_elevation(ind,:);
-
-% get lat, lon, elevation for all years
-lat  = ArgData(:,2);
-long = ArgData(:,3);
-Z    = ArgData(:,4);
 %% 
-figure(9); clf
-ax = worldmap('Argentina');
-setm(ax,'MapProjection','pcarree','MapLatLimit',[-70 80])
-
-
-
-
-
-
-
+% figure(9); clf
+% ax = worldmap('Argentina');
+% setm(ax,'MapProjection','pcarree','MapLatLimit',[-70 80])
 
 %% plot both mosi locations and station locations on map
 figure(1);clf
@@ -71,9 +81,10 @@ ax.YLim = [-60 -20];
 
 % geoshow('landareas.shp', 'FaceColor', [0.5 1.0 0.5]);
 hold on
-scatter(lat,long,'y','filled')
+scatter(Along,Alat,'y','filled')
+scatter(Blong,Blat,'b','filled')
 scatter(Tlong,Tlat,'r','filled')
-legend('mosquitos','temp stations','location','southeast')
+legend('aegypti','albo','temp stations','location','southeast')
 
 %% playing with topo data
 
@@ -83,7 +94,8 @@ legend('mosquitos','temp stations','location','southeast')
 
 %% just scatter mosi locations and elevation for Argentina
 figure(2);clf
-scatter3(lat,long,Z,'y','filled'); hold all
+scatter3(Along,Alat,Az,'y','filled'); hold all
+scatter3(Blong,Blat,Bz,'b','filled'); 
 scatter3(Tlong,Tlat,Tz,'r','filled')
 % legend('mosquitos','temp stations')
 ax = gca;
