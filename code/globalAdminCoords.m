6128447ec589f4a907b21826d44ea406fb63d5a4
jenn <jfohring@eos.ubc.ca>
2016-08-03 16:11:56 -0700

    to extract global ploygon coords

---
+++ code/globalAdminCoords.m
@@ -0,0 +1,4 @@
+% write a script to extract admin level coordinates for specified country
+% form large database. (ADMIN level 0-2) Need polygon centre coordinates,
+% and to plot polygons (Downloaded from http://www.gadm.org/version2)
+% J.Fohring Aug 2016. 
