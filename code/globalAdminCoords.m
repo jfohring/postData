304dc9c406fc8dca106cd8f028c9ec394b05d2a1
jenn <jfohring@eos.ubc.ca>
2016-08-03 17:01:32 -0700

    plot argentina admin 1

--- code/globalAdminCoords.m
+++ code/globalAdminCoords.m
@@ -2,3 +2,11 @@
 % form large database. (ADMIN level 0-2) Need polygon centre coordinates,
 % and to plot polygons (Downloaded from http://www.gadm.org/version2)
 % J.Fohring Aug 2016. 
+
+
+% learning shaperead and shapefiles (Afgahnistan)
+[S, A] = shaperead('ARG_adm1.shp', 'UseGeoCoords',true);
+
+% just plot all the admin level 1 provincial boundaries
+figure(1)
+geoshow('ARG_adm1.shp')
