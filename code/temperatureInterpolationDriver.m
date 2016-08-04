f384cbcef039bf3c8a47c9ec241c376d4c3bdee5
jenn <jfohring@eos.ubc.ca>
2016-08-04 14:41:10 -0700

    extra temp and precip data

--- code/temperatureInterpolationDriver.m
+++ code/temperatureInterpolationDriver.m
@@ -12,16 +12,37 @@ close all
 
 %% import temp station locations for Argentina
 filename = 'Argentina_2015.csv';
-year  = 2015;
-month = 1;
-D     = datetime(year,month,01,'Format','yyyy.MM.dd');
-D     = char(D,'yyyyMM');
+year     = 2015;
+month    = 1;
+D        = datetime(year,month,01,'Format','yyyy.MM.dd');
+d        = yyyymmdd(D);
+D        = char(D,'yyyyMM');
 
-
+%%
 delimiterIn = ','; %  save files as comma separated text files ('\t') for tab separated
 headerlinesIn = 1;
 
 % get a structure with headers and text
 % importdata looks at file extensions and imports accordingly
 
-T = importdata(filename,delimiterIn,headerlinesIn); 
+T = importdata(filename,delimiterIn); 
+
+% number of stations
+nstat = sum(T.data(:,4)== d);
+Lat = T.data(:,2);
+Lon = T.data(:,3);
+
+% want (TPCP (total precipitation amout per month, mm)) and (MNTM (mean temp C))
+colP = 38; % TPCP
+colT = 50; % MNTM
+
+Pdata = T.data(:,38);
+Tdata = T.data(:,50);
+
+% remove negative values
+Pdata(find(Pdata<0)) = 0;
+Tdata(find(Tdata<0)) = 0;
+
+%%
+figure(1);clf
+plot(Tdata)
