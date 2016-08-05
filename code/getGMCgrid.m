ba49cb9b40e813fe26ba888dd44a87dbf52e70e8
jenn <jfohring@eos.ubc.ca>
2016-08-04 20:44:31 -0700

    update function

--- code/getGMCgrid.m
+++ code/getGMCgrid.m
@@ -1,7 +1,12 @@
 function [Grid] = getGMCgrid(country, admin)
+% function [Grid] = getGMCgrid(country, admin)
 % generate the GCM grid with top left pixel corner lat 90, long -180
 % degrees with 0.05 degree pixel length (~5600m x 5600m)
+% -----------------------------------------------------------
+% ISO code for country should be a string.
+% Admin level is numberica
 
+%____________________________________________________________
 % pass a string that is the country ISO code. ie 'ARG'
 filename = [country '_adm' num2str(admin) '.shp'];
 
@@ -11,12 +16,12 @@ filename = [country '_adm' num2str(admin) '.shp'];
 bbox = S.BoundingBox; % [min x min y; max x max y] [min long, min lat, max long, max lat]
 
 
-GGrid.World.lat = 90:-0.05:-90;
+% extract regional mesh from big mesh
+Grid.World.lat = 90:-0.05:-90;
 Grid.World.long = -180: 0.05: 180;
 
 Grid.World.latcc = Grid.World.lat(1:end-1)-0.025;
 Grid.World.longcc = Grid.World.long(1:end-1) + 0.025;
-
 ind = find(bbox(1,2) >=   Grid.World.latcc  & Grid.World.latcc <= bbox(2,2));
 Grid.Country.latcc = Grid.World.latcc(ind);
 
