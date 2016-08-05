8ca6a3abeec79071bb007984a469b345dadd87f6
jenn <jfohring@eos.ubc.ca>
2016-08-04 20:45:13 -0700

    plot shitty average polygon center
    not accurate, from bounding box coords

--- code/globalAdminCoords.m
+++ code/globalAdminCoords.m
@@ -12,7 +12,7 @@ filename = [country '_adm' num2str(admin) '.shp'];
 bbox = S.BoundingBox; % [min x min y; max x max y] [min long, min lat, max long, max lat]
 
 
-
+%% extract regional mesh from big mesh
 Grid.World.lat = 90:-0.05:-90;
 Grid.World.long = -180: 0.05: 180;
 
@@ -24,11 +24,14 @@ Grid.Country.latcc = Grid.World.latcc(ind);
 ind = find(bbox(1,1) >=   Grid.World.longcc  & Grid.World.longcc <= bbox(2,1));
 Grid.Country.longcc = Grid.World.longcc(ind);
 
+% rough center average of country bounding box
+cent = (bbox(1,:) + bbox(2,:))/2;
 
-% just plot all the admin level 1 provincial boundaries
+
+%% just plot all the admin level 1 provincial boundaries
 figure(1),clf
 geoshow('ARG_adm0.shp')
 hold on
 plot(bbox(1,1), bbox(1,2),'r*',bbox(2,1),bbox(1,2),'r*', 'MarkerSize',5)
 plot(bbox(1,1), bbox(2,2),'r*',bbox(2,1),bbox(2,2),'r*', 'MarkerSize',5)
-
+plot(cent(1), cent(2),'bo', 'MarkerSize',5,'LineWidth',4)
