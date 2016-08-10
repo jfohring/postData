9322d889a34fd4c42a19f1fc994aa10ae5696e4e
jenn <jfohring@eos.ubc.ca>
2016-08-10 12:48:30 -0700

    create new denge driver for PR

---
+++ code/denguePuertoRicoDriver.m
@@ -0,0 +1,4 @@
+% New file for playing with Dengue Data for Puerto Rico
+
+% 1. Need to add coordinates from shape file for each province (admin 1)
+% 2. rewrite file with updated coordinates in same format as temp data
