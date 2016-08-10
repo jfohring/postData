315c0dac65568b8a3b13c3d4d3c4a6e256e82ffb
jenn <jfohring@eos.ubc.ca>
2016-08-10 14:05:56 -0700

    reads in files

--- code/denguePuertoRicoDriver.m
+++ code/denguePuertoRicoDriver.m
@@ -2,3 +2,18 @@
 
 % 1. Need to add coordinates from shape file for each province (admin 1)
 % 2. rewrite file with updated coordinates in same format as temp data
+clear
+clc
+close all
+%% Read in Dengue spread sheet
+filename = 'CDCDengue_PR_Mex.csv';
+
+delimiterIn = ','; %  save files as comma separated text files ('\t') for tab separated
+headerlinesIn = 1;
+
+D = importdata(filename,delimiterIn); 
+
+%% Read in Global Admin file for PRI
+
+
+[S, A] = shaperead('PRI_adm1.shp', 'UseGeoCoords',true);
