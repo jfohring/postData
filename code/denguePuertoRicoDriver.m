195a96e6daf3890293bcdecd44a2a73a26ad910e
jenn <jfohring@eos.ubc.ca>
2016-08-11 16:01:35 -0700

    calculate number of repeats for stations

--- code/denguePuertoRicoDriver.m
+++ code/denguePuertoRicoDriver.m
@@ -1,7 +1,6 @@
-% New file for playing with Dengue Data for Puerto Rico
+% New file for playing with Dengue Data for Puerto Rico and adding
+% occurances to the new GCM grid
 
-% 1. Need to add coordinates from shape file for each province (admin 1)
-% 2. rewrite file with updated coordinates in same format as temp data
 clear
 clc
 close all
@@ -11,8 +10,8 @@ country  = 'Puerto Rico';
 filename = 'CDCDengue.txt';
 cID = 'PRI';
 
-year  = 2015;
-month = 2;
+year  = 2010;
+month = 1;
 D     = datetime(year,month,01,'Format','yyyy.MM.dd');
 d     = yyyymmdd(D);
 D     = char(D,'yyyyMM');
@@ -31,6 +30,7 @@ Ind = find(yeardata == year & monthdata == month);
 lat = Lat(Ind);
 lon = Lon(Ind);
 
+nCases = length(Ind);
 
 %% get GCM grid for specific country
 Grid   = getGCMgrid(cID, 0,0.05);
@@ -38,14 +38,19 @@ newLat = Grid.Country.latcc;
 newLon = Grid.Country.longcc;
 [X,Y]  = meshgrid(newLon,newLat);
 
-
-%%
+%% pLot locations on map from getGCMgrid
 hold on
 scatter(lon,lat,'go','filled')
+title(['Dengue case locations. Total cases: ' num2str(nCases)])
 
 
+%% assign values to new grid (if the occuance is within the cell, that cell gets a value, else its zero
+ xy = [lat,lon];
+ [C, ia, ic] = unique(xy,'rows', 'stable');
+ nrepeats =  [ia(2:end);length(lat)+1]-ia;
+ 
 
-%% assign values to new grid (i fthe occuance is within the cell, that cell gets a value, else its zero
+ 
 
 
 
