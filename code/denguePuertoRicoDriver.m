c15dc3318c980a3dfcb05c1ea5201070243689a0
jenn <jfohring@eos.ubc.ca>
2016-08-17 15:39:03 -0700

    comments, testing for 2015 01.

--- code/denguePuertoRicoDriver.m
+++ code/denguePuertoRicoDriver.m
@@ -10,7 +10,7 @@ country  = 'Puerto Rico';
 filename = 'CDCDengue.txt';
 cID = 'PRI';
 
-year  = 2010;
+year  = 2015;
 month = 1;
 D     = datetime(year,month,01,'Format','yyyy.MM.dd');
 d     = yyyymmdd(D);
@@ -30,7 +30,7 @@ Ind = find(yeardata == year & monthdata == month);
 lat = Lat(Ind);
 lon = Lon(Ind);
 
-nCases = length(Ind);
+nCases = length(Ind); % total case per year
 
 %% get GCM grid for specific country
 Grid   = getGCMgrid(cID, 0,0.05);
@@ -45,10 +45,15 @@ title(['Dengue case locations. Total cases: ' num2str(nCases)])
 
 
 %% assign values to new grid (if the occuance is within the cell, that cell gets a value, else its zero
- xy = [lat,lon];
- [C, ia, ic] = unique(xy,'rows', 'stable');
- nrepeats =  [ia(2:end);length(lat)+1]-ia;
+ xy = [lon,lat];                            % lat and long of regions
+ [C, ia, ic] = unique(xy,'rows', 'stable'); % remove repeats
+ nDengue =  [ia(2:end);length(lat)+1]-ia;   % number of dengue cases per region
  
+ % need to deterine what GCM grid cell xy belongs to and add correct number
+ % of Dengue instances then write to corrent file
+ 
+latE = Grid.Country.lat;  % nodal grid
+lonE = Grid.Country.long; % nodal grid
 
  
 
