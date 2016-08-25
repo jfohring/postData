34b5187af14df9cb0632fdf0021a3a8533d67272
jenn <jfohring@eos.ubc.ca>
2016-08-25 12:10:13 -0700

    start demographic driver to add to combined data

---
+++ code/demographicDataDriver.m
@@ -0,0 +1,10 @@
+% read in demographic data from 2014 and create new table for a country
+
+clear
+clc
+close all
+
+%% define filename, country ID, year, month
+country  = 'Puerto Rico';
+filename = 'CDCDengue.txt';
+cID = 'PRI';
